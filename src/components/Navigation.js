2f0de3e8026174ec4cca8b1d6855d2be5423f5bc
Javier <javier.sardinas@gmail.com>
2022-08-09 17:13:08 -0500

    Add links to nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,12 @@ import '../styles/Navigation.css';
 function Navigation() {
   return (
     <nav className="navbar">
-      <a href="/">Welcome</a>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="/about">ABOUT ME</a></li>
+        <li><a href="/portfolio">PORTFOLIO</a></li>
+        <li><a href="/resume">RESUME</a></li>
+        <li><a href="/contact">CONTACT</a></li>
+      </ul>
     </nav>
   );
 }
