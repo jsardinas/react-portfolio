d4ad6c8dcd769cbdccdb1ea8e9c7a94f54bc1793
Javier <javier.sardinas@gmail.com>
2022-08-11 17:11:16 -0500

    Add About

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,13 @@ import styles from '../styles/About.css';
 
 function Header() {
   return (
-    <div className="header" style={styles}>
-      <h1>About</h1>
+    <div className='about'>
+      <h2>About me</h2>
+      <br/>
+      <img src={require("../img/bayernavatar.jpeg")} className="img-responsive img-circle margin" alt="bayern tux avatar" width="120" height="200"/>
+      <br/>
+      <h3 class="title">Full stack developer</h3>
+      <p>Aspiring web developer with a background in mathematics. About four months of industry experience includes Git, HTML, CSS, JavaScript, Bootstrap, jQuery, SQL, Node.js, express.js, MVC and ORMs. Strong skills include bathing my dogs without getting too wet (they are big dogs), eating a bag of german cookies from H-E-B without sharing and solving the Rubik Cube in under a minute.</p>
     </div>
   );
 }
