10fe3c268e7a597edc79666479307a54a9ce5326
Javier <javier.sardinas@gmail.com>
2022-08-14 10:13:16 -0500

    check for errors in contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,58 @@
 import React, { useState } from 'react';
 import '../styles/Contact.css';
 
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
+
+
 function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
+
   const [email, setEmail] = useState('');
   const [userName, setUserName] = useState('');
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
+  const inputKeys = ['userName', 'email', 'message'];
+
+  const [INPUT_NAME, INPUT_MAIL, INPUT_MSSG] = inputKeys;
+
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  const validateNonEmpty = (text) => {
+    return typeof (text) === String && text !== "";
+  }
+
+  const validateInput = {
+    [INPUT_NAME]: validateNonEmpty,
+    [INPUT_MAIL]: validateEmail,
+    [INPUT_MSSG]: validateNonEmpty
+  }
+
+  const errorMessages = {
+    [INPUT_NAME] : 'Please tell me your name',
+    [INPUT_MAIL] : 'email seems to be invalid',
+    [INPUT_MSSG] : 'Please write something'
+  };
+
+  const handleFocusOut = (e) => {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
+    console.log('validateInput:', validateInput);
+    console.log('validateInput[inoutType]:', validateInput[inputType]);
+
+
+    if (inputKeys.includes(inputType) && !validateInput[inputType](inputValue))
+      setErrorMessage(errorMessages[inputType]);
+  }
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
@@ -33,15 +66,8 @@ function Contact() {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
+    // process request
 
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
     setUserName('');
     setMessage('');
     setEmail('');
@@ -51,25 +77,27 @@ function Contact() {
     <div className='row'>
       <div className='col-lg-6 col-md-9 col-sm-12 col-12'>
         <form className="form">
-        <label for='userName'>Name</label>
-        <input
+          <label labelfor='userName'>Name</label>
+          <input
             value={userName}
             name="userName"
             onChange={handleInputChange}
             type="text"
-            placeholder="username"
+            placeholder="name"
+            onBlur={handleFocusOut}
           />
 
-          <label for='email'>Email</label>
+          <label labelfor='email'>Email</label>
           <input
             value={email}
             name="email"
             onChange={handleInputChange}
             type="email"
             placeholder="email"
+            onBlur={handleFocusOut}
           />
-          
-          <label for='message'>Message</label>
+
+          <label labelfor='message'>Message</label>
           <div>
             <textarea
               value={message}
@@ -77,6 +105,7 @@ function Contact() {
               onChange={handleInputChange}
               type="message"
               placeholder=""
+              onBlur={handleFocusOut}
             />
           </div>
           <button type="button" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
