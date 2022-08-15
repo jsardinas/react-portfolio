41813dd11946df4e2653b67e2ed95c7b33cf00d5
Javier <javier.sardinas@gmail.com>
2022-08-14 19:38:26 -0500

    Set navigation styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,10 @@ function Navigation({ currentPage, handlePageChange }) {
   return (
     <nav className="navbar">
       <ul className="nav navbar-nav navbar-right">
-        <li><a href="#about" onClick={() => handlePageChange('About')}>ABOUT ME</a></li>
-        <li><a href="#portfolio" onClick={() => handlePageChange('Portfolio')}>PORTFOLIO</a></li>
-        <li><a href="#resume" onClick={() => handlePageChange('Resume')}>RESUME</a></li>
-        <li><a href="#contact" onClick={() => handlePageChange('Contact')}>CONTACT</a></li>
+        <li><a className={currentPage==='About'?'active':'inactive'} href="#about" onClick={() => handlePageChange('About')}>ABOUT ME</a></li>
+        <li><a className={currentPage==='Portfolio'?'active':'inactive'} href="#portfolio" onClick={() => handlePageChange('Portfolio')}>PORTFOLIO</a></li>
+        <li><a className={currentPage==='Resume'?'active':'inactive'} href="#resume" onClick={() => handlePageChange('Resume')}>RESUME</a></li>
+        <li><a className={currentPage==='Contact'?'active':'inactive'} href="#contact" onClick={() => handlePageChange('Contact')}>CONTACT</a></li>
       </ul>
     </nav>
   );
